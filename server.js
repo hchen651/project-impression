f647888887b8642cdc15c0ebf8161b2532b3cb75
Henry <hchen651@gmail.com>
2020-02-26 20:20:13 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,37 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const routes = require('./routes/api');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const routes = require("./routes");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const port = process.env.PORT || 5000;
-
-//connect to the database
-mongoose.connect(process.env.DB, { useNewUrlParser: true })
-  .then(() => console.log(`Database connected successfully`))
-  .catch(err => console.log(err));
-
-//since mongoose promise is depreciated, we overide it with node's promise
-mongoose.Promise = global.Promise;
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
 
-app.use('/api', routes);
+// DB Config
+const db = require("./config/keys").mongoURI;
 
-app.use((err, req, res, next) => {
-  console.log(err);
-  next();
-});
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  console.log("in production");
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+app.use(express.static(path.join(__dirname, "client/public")));
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+// Connect to the Mongo DB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+
+// Add routes, both API and view
+app.use(routes);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
